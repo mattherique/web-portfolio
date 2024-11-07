dd8fc172fb1e0d738aae04a0ec52123abb9cefcb
Matheus Henrique de Azevedo <matheus.henriquedeazevedo@gmail.com>
2024-11-07 19:02:54 -0300

    feat: link to redirect to posts

--- src/ui/MainPage.tsx
+++ src/ui/MainPage.tsx
@@ -1,14 +1,15 @@
+'use client'
+
 import React from 'react';
 import { IoLogoLinkedin } from "react-icons/io5";
 import { IoLogoGithub } from "react-icons/io";
 import { BsJournalText } from "react-icons/bs";
 import AllPosts from '@/app/components/posts/all-posts';
-
-// src\ui\MainPage.tsx
+import Link from 'next/link';
 
 const MainPage = () => {
     return (    
-        <div className='mx-auto max-w-6xl h-full'>
+        <div className='mx-12 xl:mx-auto max-w-6xl h-full'>
             <div className='w-[65%]'>
                 <h1 className='text-5xl mt-20 text-animation'>Hey! I'm Mathew, a fullstack developer</h1>
                 <div className='mt-10 flex flex-col gap-y-4'>
@@ -20,12 +21,16 @@ const MainPage = () => {
                             <button data-dark-mode={true} className='cursor-pointer flex items-center gap-x-3 py-2 px-3 bg-[#1c1837] text-white rounded-sm hover:-mt-2 border-animation'> Linkedin
                                 <IoLogoLinkedin className='w-9 h-9 text-[#0a66c2]' />
                             </button>
-                            <button className='cursor-pointer flex items-center gap-x-3 py-2 px-3 rounded-md bg-[#1c1837] text-white shadow-md hover:-mt-2 border-animation'> Posts
-                                <BsJournalText className='w-9 h-9' />
-                            </button>
-                            <button className='cursor-pointer flex items-center gap-x-3 py-2 px-3 rounded-md bg-[#1c1837] hover:-mt-2 border-animation'> Github
-                                <IoLogoGithub className='w-9 h-9' />
-                            </button>
+                            <Link href='/posts'>
+                                <div className='cursor-pointer flex items-center gap-x-3 py-2 px-3 rounded-md bg-[#1c1837] text-white shadow-md hover:-mt-2 border-animation'> Posts
+                                    <BsJournalText className='w-9 h-9' />
+                                </div>
+                            </Link>
+                            <Link href='/posts'>
+                                <div className='cursor-pointer flex items-center gap-x-3 py-2 px-3 rounded-md bg-[#1c1837] hover:-mt-2 border-animation'> Github
+                                    <IoLogoGithub className='w-9 h-9' />
+                                </div>
+                            </Link>
                         </div>
                     </div>
                 </div>
--- src/ui/Navbar.tsx
+++ src/ui/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <nav className='h-16 flex items-center justify-between mx-auto max-w-6xl'>
+        <nav className='h-16 flex items-center justify-between mx-12 xl:mx-auto max-w-6xl'>
             <p className='text-lg'>Matheus Azevedo</p>
             <div className='flex gap-x-10'>
                 <button className='hover:bg-slate-800 py-2 px-3 rounded-md transition-all'>
